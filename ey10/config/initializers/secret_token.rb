8f21dcd6dafeac414f3179904b35a9f919070fa5
Dan Bikle <dan@getworksimple.com>
2012-12-03 23:18:54 +0000

    c1

---
+++ ey10/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ey10::Application.config.secret_token = '831367ef4bfb93057ce9faa770bc288827d63e94c13e1e849c5463eb3564e547b96b12b194c6d6d1a7bd12306153951f7e0077f375089a6ec42a32bbb2eac360'
